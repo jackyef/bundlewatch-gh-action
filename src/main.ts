ada13fc12ee1a4f28893fcd9a2bda2b40af66064
Jacky Efendi <jacky.efendi1@gmail.com>
2019-12-23 12:20:20 +0700

    feat: initial setup
    * feat: Setup bundlesize GH action
    
    * fix: workflow yml
    
    * fix: use node12 because node10 is not supported
    
    * fix: Changed wrong build script
    
    * fix: Fix main.ts
    
    * test
    
    * switch to bundlewatch
    
    * Use npm
    
    * some update
    
    * added missing var

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,50 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    let output = '';
+    let error = '';
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    const options: any = {};
 
-    core.setOutput('time', new Date().toTimeString());
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        output += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        error += data.toString();
+      }
+    };
+
+    const buildScript = core.getInput('build-script');
+    const bundlesizeGithubToken = core.getInput('bundlewatch-github-token');
+
+    const githubPayload = github.context.payload;
+
+    if (!githubPayload) throw new Error('Failed when trying to get PR information');
+    
+    const commitSHA = githubPayload.pull_request ? githubPayload.pull_request.head.sha : '';
+    const branchName = githubPayload.pull_request ? githubPayload.pull_request.head.ref : '';
+    const prTitle = githubPayload.pull_request ? githubPayload.pull_request.title : '';
+    const repoOwner = githubPayload.repository ? githubPayload.repository.owner.login : '';
+    const repoName = githubPayload.repository ? githubPayload.repository.name : '';
+
+    core.exportVariable('CI_REPO_OWNER', repoOwner);
+    core.exportVariable('CI_REPO_NAME', repoName);
+    core.exportVariable('CI_COMMIT_SHA', commitSHA);
+    core.exportVariable('CI_BRANCH', branchName);
+    core.exportVariable('BUNDLEWATCH_GITHUB_TOKEN', bundlesizeGithubToken);
+
+    console.log(`Running: npm run ${buildScript}`);
+    await exec.exec(`npm run ${buildScript}`, undefined);
+
+    console.log(`Running: bundlewatch`);
+    await exec.exec(`npx bundlewatch`, undefined, options);
+    if (output) console.info(output);
+    if (error) throw new Error(error);
+
   } catch (error) {
     core.setFailed(error.message);
   }
