d08b7ebcfff4f1ff73131484bd4c53011344d4f6
Jacky Efendi <jacky.efendi1@gmail.com>
2020-11-17 18:55:41 +0700

    fix: remove import resolver

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript'; // we use 3.1.1 because of https://github.com/rollup/plugins/issues/287#issuecomment-645100737
 import json from '@rollup/plugin-json';
-import importResolver from 'rollup-plugin-import-resolver';
 import path from 'path';
 
 const nodeModulesDir = path.resolve(__dirname, 'node_modules');
@@ -16,20 +15,6 @@ export default {
   plugins: [
     // Allow json resolution
     json(),
-
-    importResolver({
-      // a list of file extensions to check, default = ['.js']
-      extensions: ['.js'],
-      // a list of aliases, default = {}
-      alias: {
-        // force use modern tslib
-        'tslib': 'tslib.es6.js',
-      },
-      // index file name without extension, default = 'index'
-      indexFile: 'index',
-      // path to node_modules dir, default = ./node_modules
-      modulesDir: './node_modules',
-    }),
 
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
